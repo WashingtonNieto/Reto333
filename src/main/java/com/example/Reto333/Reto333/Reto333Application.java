fc102b14dd25d2bfa8ef273479e3d4e207ccf8fe
Washington Nieto <washington_nieto@hotmail.com>
2021-10-20 20:13:36 -0500

    todo

---
+++ src/main/java/com/example/Reto333/Reto333/Reto333Application.java
@@ -0,0 +1,13 @@
+package com.example.Reto333.Reto333;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto333Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto333Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Reto333/Reto333/Reto333ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reto333.Reto333;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto333ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
