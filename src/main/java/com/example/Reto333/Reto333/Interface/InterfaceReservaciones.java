1fc15141dc25afbf4ec7a72f870126a35b9e72cb
Washington Nieto <washington_nieto@hotmail.com>
2021-10-27 18:31:17 -0500

    Reto5-1

--- src/main/java/com/example/Reto333/Reto333/Interface/InterfaceReservaciones.java
+++ src/main/java/com/example/Reto333/Reto333/Interface/InterfaceReservaciones.java
@@ -3,5 +3,13 @@ package com.example.Reto333.Reto333.Interface;
 import com.example.Reto333.Reto333.Entity.Reservaciones;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface InterfaceReservaciones extends CrudRepository <Reservaciones,Integer>{
+
+    public List<Reservaciones> findAllByStarDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+
+    public List<Reservaciones> findAllByD
+
 }
